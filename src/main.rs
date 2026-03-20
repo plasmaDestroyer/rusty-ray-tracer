42d0becc53223b227996b6193032570b4a16fc25
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-20 22:42:35 +0530

    Rays, a Simple Camera, and Background

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,64 @@
 mod color;
+mod ray;
 mod vec3;
 
 use color::Color;
 use color::write_color;
+use ray::Ray;
 use std::io::Write;
+use vec3::Point3;
+use vec3::Vec3;
+use vec3::unit_vector;
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = unit_vector(r.direction());
+    let a = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
 
-    let image_width: u32 = 256;
-    let image_height: u32 = 256;
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: u32 = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    let mut image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
+    image_height = if image_height < 1 { 1 } else { image_height };
+
+    // Camera
+
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64) / (image_height as f64);
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width;
+    let pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     //Render
 
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
-    for i in 0..image_height {
-        eprint!("\rScanlines remaining: {} ", image_height - i);
+    for j in 0..image_height {
+        eprint!("\rScanlines remaining: {} ", image_height - j);
         std::io::stderr().flush().unwrap();
-        for j in 0..image_width {
-            let pixel_color = Color::new(
-                (j as f64) / ((image_width - 1) as f64),
-                (i as f64) / ((image_height - 1) as f64),
-                0.0,
-            );
+        for i in 0..image_width {
+            let pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+
+            let pixel_color = ray_color(r);
             write_color(&mut std::io::stdout(), &pixel_color);
         }
     }
