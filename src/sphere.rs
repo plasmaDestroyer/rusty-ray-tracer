ddc547351a30e6f3413f099f63f028832caf19b0
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-21 22:07:32 +0530

    abstraction for hittable objects

---
+++ src/sphere.rs
@@ -0,0 +1,54 @@
+use crate::hittable::HitRecord;
+use crate::hittable::Hittable;
+use crate::ray::Ray;
+use crate::vec3::Point3;
+use crate::vec3::dot;
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: &Point3, radius: f64) -> Self {
+        Sphere {
+            center: *center,
+            radius: f64::max(0.0, radius),
+        }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        let oc = self.center - *r.origin();
+        let a = r.direction().length_squared();
+        let h = dot(r.direction(), &oc);
+        let c = oc.length_squared() - self.radius * self.radius;
+
+        let discriminant = h * h - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        // Find the nearest root that lies in the acceptable range.
+        let mut root = (h - sqrtd) / a;
+        if root <= ray_tmin || ray_tmax <= root {
+            root = (h + sqrtd) / a;
+            if root <= ray_tmin || ray_tmax <= root {
+                return None;
+            }
+        }
+
+        let t = root;
+        let p = r.at(t);
+        let normal = (p - self.center) / self.radius;
+
+        Some(HitRecord {
+            t: t,
+            p: p,
+            normal: normal,
+        })
+    }
+}
