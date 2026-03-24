c3bbc089b4380172341d8401ed65a6983133a81a
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-25 03:36:21 +0530

    use gamma correction for accurate color intensity

--- src/color.rs
+++ src/color.rs
@@ -3,10 +3,23 @@ use crate::vec3::Vec3;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
+}
+
 pub fn write_color(out: &mut impl std::io::Write, pixel_color: &Color) {
-    let r: f64 = pixel_color.x();
-    let g: f64 = pixel_color.y();
-    let b: f64 = pixel_color.z();
+    let mut r: f64 = pixel_color.x();
+    let mut g: f64 = pixel_color.y();
+    let mut b: f64 = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     let intensity = Interval::new(0.000, 0.999);
